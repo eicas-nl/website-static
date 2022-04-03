84bb2d388c23ece963de9498a61c03db473ef491
Arnout Engelen <arnout@bzzt.net>
2022-04-03 11:12:53 +0200

    events

--- mirror.sh
+++ mirror.sh
@@ -2,7 +2,7 @@
 
 # skipping word-vriend as that requires a form
 # skipping nu-te-zien as that is a redirect
-for page in franck-gribling bezoek vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-connected-partner hegiusfonds virtual-eggs-schetsen-versen space-traps thin-as-air-abundant-emptiness tij-tijd-en-tijdelijkheid tegendraads symposium-franck-gribling boekpresentatie-ellen-korth lezing-jonas-snijder ik-hoorde-dat-er-geluisterd-werd; do
+for page in frits-grimmelikhuizen-dada de-macht-van-kunst franck-gribling bezoek vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-connected-partner hegiusfonds virtual-eggs-schetsen-versen space-traps thin-as-air-abundant-emptiness tij-tijd-en-tijdelijkheid tegendraads symposium-franck-gribling boekpresentatie-ellen-korth lezing-jonas-snijder ik-hoorde-dat-er-geluisterd-werd; do
     mkdir $page 2>/dev/null
     curl localhost:8000/$page > $page/index.html
     git add $page/index.html
