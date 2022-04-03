546ada24f4bbf40b6d060aaffc63c1d3783782a7
Arnout Engelen <arnout@bzzt.net>
2022-04-03 11:00:15 +0200

    update

--- mirror.sh
+++ mirror.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-for page in bezoek nu-te-zien vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop; do
-    mkdir $page
+for page in bezoek nu-te-zien vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-vriend word-connected-partner hegiusfonds; do
+    mkdir $page 2>/dev/null
     curl localhost:8000/$page > $page/index.html
     git add $page/index.html
 done
