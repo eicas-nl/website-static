16f9e26b67f1598298707e576a1877ddf0351600
Arnout Engelen <arnout@bzzt.net>
2022-04-03 11:02:22 +0200

    virtual-eggs-schetsen-versen

--- mirror.sh
+++ mirror.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-for page in bezoek nu-te-zien vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-vriend word-connected-partner hegiusfonds; do
+# skipping word-vriend as that requires a form
+for page in bezoek nu-te-zien vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-connected-partner hegiusfonds virtual-eggs-schetsen-versen; do
     mkdir $page 2>/dev/null
     curl localhost:8000/$page > $page/index.html
     git add $page/index.html
