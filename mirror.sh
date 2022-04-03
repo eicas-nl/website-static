4af8bed358d6cbb2e323fb614be6388943bdc3cf
Arnout Engelen <arnout@bzzt.net>
2022-04-03 11:12:10 +0200

    redirect 'nu te zien'

--- mirror.sh
+++ mirror.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # skipping word-vriend as that requires a form
-for page in franck-gribling bezoek nu-te-zien vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-connected-partner hegiusfonds virtual-eggs-schetsen-versen space-traps thin-as-air-abundant-emptiness tij-tijd-en-tijdelijkheid tegendraads symposium-franck-gribling boekpresentatie-ellen-korth lezing-jonas-snijder ik-hoorde-dat-er-geluisterd-werd; do
+# skipping nu-te-zien as that is a redirect
+for page in franck-gribling bezoek vaste-collectie agenda kids nieuwsbrief in-het-nieuws archief steun-eicas over-eicas organisatie vacatures anbi privacy shop charkow word-connected-partner hegiusfonds virtual-eggs-schetsen-versen space-traps thin-as-air-abundant-emptiness tij-tijd-en-tijdelijkheid tegendraads symposium-franck-gribling boekpresentatie-ellen-korth lezing-jonas-snijder ik-hoorde-dat-er-geluisterd-werd; do
     mkdir $page 2>/dev/null
     curl localhost:8000/$page > $page/index.html
     git add $page/index.html
